e047c838f2a51d7b49d8314a76cee5012adf3de6
mudra8 <137314311+mudra8@users.noreply.github.com>
2023-09-17 16:35:59 -0700

    project changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,11 +33,18 @@ navLink.forEach((n) => n.addEventListener("click", linkAction));
 const skillsContent = document.getElementsByClassName("skills__content"),
   skillsHeader = document.querySelectorAll(".skills__header");
 
+
 function toggleSkills() {
   let itemClass = this.parentNode.className;
+  /*
   for (i = 0; i < skillsContent.length; i++) {
     skillsContent[i].className = "skills__content skills__close";
+  }*/
+
+  if (itemClass === "skills__content skills__open") {
+    this.parentNode.className = "skills__content skills__close";
   }
+
   if (itemClass === "skills__content skills__close") {
     this.parentNode.className = "skills__content skills__open";
   }
